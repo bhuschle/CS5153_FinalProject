434f4dc367e52aa5fb70ab189e4462026a2d8764
EthanChappel <EthanChappel@users.noreply.github.com>
2022-04-11 21:53:28 -0500

    Add Users table to database init script

--- initdb.sql
+++ initdb.sql
@@ -8,3 +8,17 @@ CREATE TABLE products (
     product_description TEXT,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email varchar(200),
+    user_password varchar(100),
+    user_address varchar(100),
+    user_city varchar(50),
+    user_state varchar(50),
+    user_country varchar(50),
+    user_zip_code varchar(5),
+    PRIMARY KEY(id)
+)
