f27267c44b0cc3b37a908b4b48bbb1178ce50f88
EthanChappel <EthanChappel@users.noreply.github.com>
2022-04-12 16:18:56 -0500

    Make some columns mandatory

--- initdb.sql
+++ initdb.sql
@@ -1,7 +1,7 @@
 CREATE TABLE products (
     id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
-    price DECIMAL(2) NOT NULL,
+    price DECIMAL(8, 2) NOT NULL,
     image1_path VARCHAR(4096),
     image2_path VARCHAR(4096),
     image3_path VARCHAR(4096),
@@ -11,10 +11,10 @@ CREATE TABLE products (
 
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    email varchar(200),
-    user_password varchar(100),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email varchar(200) NOT NULL,
+    user_password varchar(100) NOT NULL,
     user_address varchar(100),
     user_city varchar(50),
     user_state varchar(50),
