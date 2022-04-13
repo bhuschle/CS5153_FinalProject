63572109b244552e7de642a96502976eaa3694cb
EthanChappel <EthanChappel@users.noreply.github.com>
2022-04-12 19:57:30 -0500

    Add functions for users to database interface.

--- js/database.js
+++ js/database.js
@@ -10,8 +10,12 @@ const knex = require('knex')({
 });
 
 
+const productsTableName = 'products'
+const usersTableName = 'users'
+
+
 function addProduct(name, price, description = "") {
-    knex('products')
+    knex(productsTableName)
         .insert({
             product_name: name,
             price: price,
@@ -23,11 +27,41 @@ module.exports.addProduct = addProduct;
 
 
 function removeProduct(id) {
-    knex('products')
-        .del()
+    knex(productsTableName)
         .where({
           'id': id
-        }).then(()=>{});
+    })
+    .del()
+    .then(()=>{});
 }
 
 module.exports.removeProduct = removeProduct;
+
+
+function addUser(firstName, lastName, emailAddress, password, address, city, state, country, zipCode) {
+  knex(usersTableName)
+        .insert({
+            first_name: firstName,
+            last_name: lastName,
+            email: emailAddress,
+            user_password: password,
+            user_address: address,
+            user_city: city,
+            user_state: state,
+            user_country: country,
+            user_zip_code: zipCode
+        }).then(()=>{})
+}
+
+module.exports.addUser = addUser
+
+
+function removeUser(id) {
+  knex(usersTableName)
+      .del()
+      .where({
+        'id': id
+      }).then(()=>{});
+}
+
+module.exports.removeUser = removeUser
