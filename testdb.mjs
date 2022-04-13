071c67c934377ccaf83dfb4a13eca20c304b74ed
EthanChappel <EthanChappel@users.noreply.github.com>
2022-04-12 19:58:20 -0500

    Update test module to add and remove users from db

--- testdb.mjs
+++ testdb.mjs
@@ -1,7 +1,18 @@
 import * as database from './js/database.js';
 
+
 database.addProduct("test1", 1.99, "A product.");
 database.addProduct("test2", 2.99, "Another product.");
 database.addProduct("test3", 3.99, "Another product.");
+console.log("Products added to database.");
 
 database.removeProduct(2)
+console.log("Removed product from database.");
+
+
+database.addUser("Ethan", "Chappel", "ethan.chappel@example.edu", "password", "1 UTSA Blvd.", "San Antonio", "Texas", "United States of America", "78249")
+database.addUser("John", "Doe", "john.doe@example.edu", "password", "1 UTSA Blvd.", "San Antonio", "Texas", "United States of America", "78249")
+console.log("Users added to database.");
+
+database.removeUser(2)
+console.log("Removed user from database.");
