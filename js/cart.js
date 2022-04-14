4de8a8e7b2ec26a0d566e6dc96f35bf405a0a222
Brandon Huschle <brandon.huschle@gmail.com>
2022-04-14 17:15:55 -0500

    Added alert box

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,6 @@ let productImage = document.getElementById("productImage");
 let productPrice = document.getElementById("productPrice");
 let productName = document.getElementById("productName");
 
-productImage.src = urlParams.get('image');
-productName.innerText = urlParams.get('name');
-productPrice.innerText = `\$${urlParams.get('price')}`;
+productImage.src = urlParams.get("image");
+productName.innerText = urlParams.get("name");
+productPrice.innerText = `\$${urlParams.get("price")}`;
