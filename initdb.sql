5a6f3adf3a8ba0203a3da19b58a8cc72c95c30bc
Brandon Huschle <brandon.huschle@gmail.com>
2022-04-26 14:50:04 -0500

    fixed docker, changed products db

--- initdb.sql
+++ initdb.sql
@@ -2,10 +2,9 @@ CREATE TABLE products (
     id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
     price DECIMAL(8, 2) NOT NULL,
-    image1_path VARCHAR(4096),
-    image2_path VARCHAR(4096),
-    image3_path VARCHAR(4096),
-    product_description TEXT,
+    image_path VARCHAR(4096),
+    image_alt VARCHAR(100),
+    product_desc TEXT,
     PRIMARY KEY(id)
 );
 
--- testdb.mjs
+++ testdb.mjs
@@ -1,5 +1,4 @@
-import * as database from './js/database.js';
-
+import * as database from "./js/database.js";
 
 await database.addProduct("test1", 1.99, "A product.");
 await database.addProduct("test2", 2.99, "Another product.");
@@ -7,18 +6,38 @@ await database.addProduct("test3", 3.99, "Another product.");
 console.log("Products added to database.");
 
 console.log(await database.searchProductsByName("test"));
-console.log(await database.searchProductsByName("test", [0.00, 3.00]));
+console.log(await database.searchProductsByName("test", [0.0, 3.0]));
 
-await database.removeProduct(2)
+await database.removeProduct(2);
 console.log("Removed product from database.");
 
 console.log(await database.searchProductsByName("test"));
 
-database.addUser("Ethan", "Chappel", "ethan.chappel@example.edu", "password", "1 UTSA Blvd.", "San Antonio", "Texas", "United States of America", "78249")
-database.addUser("John", "Doe", "john.doe@example.edu", "password", "1 UTSA Blvd.", "San Antonio", "Texas", "United States of America", "78249")
+database.addUser(
+  "Ethan",
+  "Chappel",
+  "ethan.chappel@example.edu",
+  "password",
+  "1 UTSA Blvd.",
+  "San Antonio",
+  "Texas",
+  "United States of America",
+  "78249"
+);
+database.addUser(
+  "John",
+  "Doe",
+  "john.doe@example.edu",
+  "password",
+  "1 UTSA Blvd.",
+  "San Antonio",
+  "Texas",
+  "United States of America",
+  "78249"
+);
 console.log("Users added to database.");
 
-console.log(await database.getUserById(1));
+//console.log(await database.getUserById(1));
 
-database.removeUser(2)
-console.log("Removed user from database.");
+//database.removeUser(2)
+// console.log("Removed user from database.");
