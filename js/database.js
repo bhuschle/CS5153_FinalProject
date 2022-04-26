ece9626b9faf7027d2cadc6242b8fe3855e45872
Brandon Huschle <brandon.huschle@gmail.com>
2022-04-26 11:59:16 -0500

    edited altText

--- js/database.js
+++ js/database.js
@@ -1,69 +1,77 @@
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      host : 'db',
-      port : 3306,
-      user : 'server',
-      password : 'qwertyuiop',
-      database : 'store'
-    }
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: "db",
+    port: 3306,
+    user: "server",
+    password: "qwertyuiop",
+    database: "store",
+  },
 });
 
-
-const productsTableName = 'products'
-const usersTableName = 'users'
-
+const productsTableName = "products";
+const usersTableName = "users";
 
 function addProduct(name, price, description = "") {
-    knex(productsTableName)
-        .insert({
-            prod_name: name,
-            price: price,
-            prod_descr: description,
-            prod_img: image,
-            prod_alt_txt: alt text
-        }).then(()=>{})
+  knex(productsTableName)
+    .insert({
+      prod_name: name,
+      price: price,
+      prod_descr: description,
+      prod_img: image,
+      prod_alt_txt: altText,
+    })
+    .then(() => {});
 }
 
 module.exports.addProduct = addProduct;
 
-
 function removeProduct(id) {
-    knex(productsTableName)
-        .where({
-          'id': id
+  knex(productsTableName)
+    .where({
+      id: id,
     })
     .del()
-    .then(()=>{});
+    .then(() => {});
 }
 
 module.exports.removeProduct = removeProduct;
 
-
-function addUser(firstName, lastName, emailAddress, password, address, city, state, country, zipCode) {
+function addUser(
+  firstName,
+  lastName,
+  emailAddress,
+  password,
+  address,
+  city,
+  state,
+  country,
+  zipCode
+) {
   knex(usersTableName)
-        .insert({
-            first_name: firstName,
-            last_name: lastName,
-            email: emailAddress,
-            user_password: password,
-            user_address: address,
-            user_city: city,
-            user_state: state,
-            user_country: country,
-            user_zip_code: zipCode
-        }).then(()=>{})
+    .insert({
+      first_name: firstName,
+      last_name: lastName,
+      email: emailAddress,
+      user_password: password,
+      user_address: address,
+      user_city: city,
+      user_state: state,
+      user_country: country,
+      user_zip_code: zipCode,
+    })
+    .then(() => {});
 }
 
-module.exports.addUser = addUser
-
+module.exports.addUser = addUser;
 
 function removeUser(id) {
   knex(usersTableName)
-      .del()
-      .where({
-        'id': id
-      }).then(()=>{});
+    .del()
+    .where({
+      id: id,
+    })
+    .then(() => {});
 }
 
-module.exports.removeUser = removeUser
+module.exports.removeUser = removeUser;
