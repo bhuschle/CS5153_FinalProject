13cb1b121825cdbba8b7d8e3389ac4631f2eec69
EthanChappel <EthanChappel@users.noreply.github.com>
2022-04-30 18:13:22 -0500

    Use handlebars as template engine

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
 const database = require("./js/database.js");
 const path = require("path");
 const express = require("express");
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 8080;
 
+
+app.engine('html', handlebars.engine());
+
+app.set('view engine', 'handlebars');
+app.set('views', './pages');
 
 app.use("/static/img", express.static("img"));
 app.use("/static/js", express.static("js"));
 
 
 app.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname, "indexv2.html"));
+    response.render('indexv2.html', {layout: false});
 });
 
 app.get("/accessories", (request, response) => {
-    response.sendFile(path.join(__dirname, "pages", "Accessories.html"));
+    response.render('Accesories.html', {layout: false});
 });
 
 app.get("/account", (request, response) => {
-    response.sendFile(path.join(__dirname, "pages", "Account.html"));
+    response.render('Account.html', {layout: false});
 });
 
 app.get("/cart", (request, response) => {
-    response.sendFile(path.join(__dirname, "pages", "cart.html"));
+    response.render('cart.html', {layout: false});
 });
 
 app.get("/computers", (request, response) => {
-    response.sendFile(path.join(__dirname, "pages", "Computers.html"));
+    response.render('Computers.html', {layout: false});
 });
 
 app.get("/phones", (request, response) => {
-    response.sendFile(path.join(__dirname, "pages", "Phones.html"));
+    response.render('Phones.html', {layout: false});
 });
 
 app.get("/product", (request, response) => {
-    response.sendFile(path.join(__dirname, "pages", "product.html"));
+    response.render('product.html', {layout: false});
 });
 
 app.get("/tablets", (request, response) => {
-    response.sendFile(path.join(__dirname, "pages", "Tablets.html"));
+    response.render('Tablets.html', {layout: false});
 });
 
 
