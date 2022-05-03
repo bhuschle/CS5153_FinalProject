d74599a991865ae887652c521038d91ebbfffd37
Brandon Huschle <brandon.huschle@gmail.com>
2022-05-03 10:14:11 -0500

    added needed pages

--- app.js
+++ app.js
@@ -18,13 +18,15 @@ const scriptPath = "/static/js";
 
 app.use(imagePath, express.static("img"));
 app.use(scriptPath, express.static("js"));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(session({
-  secret: "Well-kept secret",
-  resave: false,
-  saveUninitialized: true,
-  cookie: {secure: false},
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "Well-kept secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 const commonUrlPath = "/common";
 const v1UrlPath = "";
@@ -34,9 +36,9 @@ const v1ViewsPath = "./pages";
 const v2ViewsPath = "./v2";
 
 const commonBaseContext = {
-    commonUrlRoot: commonUrlPath,
-    imageRoot: imagePath,
-}
+  commonUrlRoot: commonUrlPath,
+  imageRoot: imagePath,
+};
 
 const v1BaseContext = {
   ...commonBaseContext,
@@ -45,25 +47,70 @@ const v1BaseContext = {
 };
 
 const v2BaseContext = {
-    ...commonBaseContext,
-    urlRoot: v2UrlPath,
+  ...commonBaseContext,
+  urlRoot: v2UrlPath,
 };
 
-
 function filterAndSort(products, brands, sortBy) {
-    products = products.filter(element => brands.includes(element['brand']))
-    if (sortBy === "Low-to-high") {
-        products.sort((a, b) => a['price'] - b['price']);
-    }
-    else if (sortBy === "High-to-low") {
-        products.sort((a, b) => b['price'] - a['price']);
-    }
-    else if (sortBy === "Best sellers") {
-        products.sort((a, b) => b['sold_count'] - a['sold_count']);
-    }
-    return products;
+  products = products.filter((element) => brands.includes(element["brand"]));
+  if (sortBy === "Low-to-high") {
+    products.sort((a, b) => a["price"] - b["price"]);
+  } else if (sortBy === "High-to-low") {
+    products.sort((a, b) => b["price"] - a["price"]);
+  } else if (sortBy === "Best sellers") {
+    products.sort((a, b) => b["sold_count"] - a["sold_count"]);
+  }
+  return products;
 }
 
+app.get(`${v1UrlPath}/locations`, (request, response) => {
+  response.render(`${v1ViewsPath}/locations.html`, {
+    ...v1BaseContext,
+    layout: false,
+  });
+});
+
+app.get(`${v1UrlPath}/signin`, (request, response) => {
+  let signInFailed = request.session.signInFailed;
+  request.session.signInFailed = null;
+  response.render(`${v1ViewsPath}/signin.html`, {
+    ...v2BaseContext,
+    layout: "./auth.html",
+    signInFailed: signInFailed,
+  });
+});
+
+app.get(`${v1UrlPath}/about`, (request, response) => {
+  response.render(`${v1ViewsPath}/aboutus.html`, {
+    ...v1BaseContext,
+    layout: false,
+  });
+});
+
+app.get(`${v1UrlPath}/signout`, (request, response) => {
+  request.session.destroy((error) => {});
+  response.render(`${v1ViewsPath}/signedout.html`, {
+    ...v1BaseContext,
+    layout: "./auth.html",
+  });
+});
+
+app.get(`${v1UrlPath}/signup`, (request, response) => {
+  let passwordMismatch = request.session.passwordMismatch;
+  request.session.passwordMismatch = null;
+  response.render(`${v1ViewsPath}/createaccount.html`, {
+    ...v1BaseContext,
+    layout: "./auth.html",
+    passwordMismatch: passwordMismatch,
+  });
+});
+
+app.get(`${v1UrlPath}/signup/success`, (request, response) => {
+  response.render(`${v1ViewsPath}/createaccountsuccess.html`, {
+    ...v1BaseContext,
+    layout: "./auth.html",
+  });
+});
 
 // V1 INFORMATION
 
@@ -77,7 +124,7 @@ app.get(`${v1UrlPath}/`, (request, response) => {
 });
 
 app.get(`${v1UrlPath}/account`, async (request, response) => {
-  let user = await database.getUser({id: request.session.userId});
+  let user = await database.getUser({ id: request.session.userId });
 
   response.render(`${v1ViewsPath}/account.html`, {
     ...v1BaseContext,
@@ -223,8 +270,8 @@ app.get(`${v1UrlPath}/music`, async (request, response) => {
 });
 
 app.get(`${v1UrlPath}/signout`, (request, response) => {
-  request.session.destroy((error)=>{});
-  response.redirect(`${v1UrlPath}/`)
+  request.session.destroy((error) => {});
+  response.redirect(`${v1UrlPath}/`);
   /*response.render(
     `${v1ViewsPath}/signedout.html`,
     {
@@ -234,19 +281,15 @@ app.get(`${v1UrlPath}/signout`, (request, response) => {
   );*/
 });
 
-
 // V2 INFORMATION
 
 app.get(`${v2UrlPath}`, (request, response) => {
-    response.render(
-        `${v2ViewsPath}/index.html`,
-        {
-            ...v2BaseContext,
-            userFirstName: request.session.firstName,
-            loggedIn: request.session.loggedIn,
-            layout: './basev2.html',
-        }
-    );
+  response.render(`${v2ViewsPath}/index.html`, {
+    ...v2BaseContext,
+    userFirstName: request.session.firstName,
+    loggedIn: request.session.loggedIn,
+    layout: "./basev2.html",
+  });
 });
 
 app.get(`${v2UrlPath}/account`, (request, response) => {
@@ -258,277 +301,244 @@ app.get(`${v2UrlPath}/cart`, (request, response) => {
 });
 
 app.get(`${v2UrlPath}/computers`, async (request, response) => {
-    let subcategoryId = "computers";
-    let products = await database.getProducts({ subcategory: subcategoryId });
-    brands = new Set(products.map((x) => x["brand"]));
+  let subcategoryId = "computers";
+  let products = await database.getProducts({ subcategory: subcategoryId });
+  brands = new Set(products.map((x) => x["brand"]));
 
-    if (Object.keys(request.query).length !== 0) {
-        let filterBrands = Object.keys(request.query);
-        delete filterBrands['sortby'];
-        products = filterAndSort(products, filterBrands, request.query['sortby']);
-    }
+  if (Object.keys(request.query).length !== 0) {
+    let filterBrands = Object.keys(request.query);
+    delete filterBrands["sortby"];
+    products = filterAndSort(products, filterBrands, request.query["sortby"]);
+  }
 
-    response.render(
-        `${v2ViewsPath}/productpageV2.html`,
-        {
-            ...v2BaseContext,
-            userFirstName: request.session.firstName,
-            loggedIn: request.session.loggedIn,
-            layout: './basev2.html',
-            products: products,
-            productsString: JSON.stringify(products),
-            category: "Computers",
-            subcategoryId: subcategoryId,
-            brands: brands,
-        }
-    );
+  response.render(`${v2ViewsPath}/productpageV2.html`, {
+    ...v2BaseContext,
+    userFirstName: request.session.firstName,
+    loggedIn: request.session.loggedIn,
+    layout: "./basev2.html",
+    products: products,
+    productsString: JSON.stringify(products),
+    category: "Computers",
+    subcategoryId: subcategoryId,
+    brands: brands,
+  });
 });
 
 app.get(`${v2UrlPath}/iphone`, async (request, response) => {
-    let subcategoryId = "computers";
-    let products = await database.getProducts({ subcategory: subcategoryId });
-    brands = new Set(products.map((x) => x["brand"]));
+  let subcategoryId = "computers";
+  let products = await database.getProducts({ subcategory: subcategoryId });
+  brands = new Set(products.map((x) => x["brand"]));
 
-    if (Object.keys(request.query).length !== 0) {
-        let filterBrands = Object.keys(request.query);
-        delete filterBrands['sortby'];
-        products = filterAndSort(products, filterBrands, request.query['sortby']);
-    }
-    
-    response.render(
-        `${v2ViewsPath}/productpageV2.html`,
-        {
-            ...v2BaseContext,
-            userFirstName: request.session.firstName,
-            loggedIn: request.session.loggedIn,
-            layout: './basev2.html',
-            products: products,
-            productsString: JSON.stringify(products),
-            category: "iPhone",
-            subcategoryId, subcategoryId,
-            brands: brands,
-        }
-    );
+  if (Object.keys(request.query).length !== 0) {
+    let filterBrands = Object.keys(request.query);
+    delete filterBrands["sortby"];
+    products = filterAndSort(products, filterBrands, request.query["sortby"]);
+  }
+
+  response.render(`${v2ViewsPath}/productpageV2.html`, {
+    ...v2BaseContext,
+    userFirstName: request.session.firstName,
+    loggedIn: request.session.loggedIn,
+    layout: "./basev2.html",
+    products: products,
+    productsString: JSON.stringify(products),
+    category: "iPhone",
+    subcategoryId,
+    subcategoryId,
+    brands: brands,
+  });
 });
 
 app.get(`${v2UrlPath}/samsung`, async (request, response) => {
-    let subcategoryId = "samsung";
-    let products = await database.getProducts({ subcategory: subcategoryId });
-    brands = new Set(products.map((x) => x["brand"]));
+  let subcategoryId = "samsung";
+  let products = await database.getProducts({ subcategory: subcategoryId });
+  brands = new Set(products.map((x) => x["brand"]));
 
-    if (Object.keys(request.query).length !== 0) {
-        let filterBrands = Object.keys(request.query);
-        delete filterBrands['sortby'];
-        products = filterAndSort(products, filterBrands, request.query['sortby']);
-    }
+  if (Object.keys(request.query).length !== 0) {
+    let filterBrands = Object.keys(request.query);
+    delete filterBrands["sortby"];
+    products = filterAndSort(products, filterBrands, request.query["sortby"]);
+  }
 
-    response.render(
-        `${v2ViewsPath}/productpageV2.html`,
-        {
-            ...v2BaseContext,
-            userFirstName: request.session.firstName,
-            loggedIn: request.session.loggedIn,
-            layout: './basev2.html',
-            products: products,
-            productsString: JSON.stringify(products),
-            category: "Samsung",
-            subcategoryId: subcategoryId,
-            brands: brands,
-        }
-    );
+  response.render(`${v2ViewsPath}/productpageV2.html`, {
+    ...v2BaseContext,
+    userFirstName: request.session.firstName,
+    loggedIn: request.session.loggedIn,
+    layout: "./basev2.html",
+    products: products,
+    productsString: JSON.stringify(products),
+    category: "Samsung",
+    subcategoryId: subcategoryId,
+    brands: brands,
+  });
 });
 
 app.get(`${v2UrlPath}/product`, (request, response) => {
   response.render(`${v2ViewsPath}/product.html`, { layout: false });
 });
 
-app.get(`${v2UrlPath}/tablets`,
-    bodyParser.urlencoded(),
-    async (request, response) => {
-        let subcategoryId = "tablets";
-        let products = await database.getProducts({ subcategory: subcategoryId });
-        brands = new Set(products.map((x) => x["brand"]));
+app.get(
+  `${v2UrlPath}/tablets`,
+  bodyParser.urlencoded(),
+  async (request, response) => {
+    let subcategoryId = "tablets";
+    let products = await database.getProducts({ subcategory: subcategoryId });
+    brands = new Set(products.map((x) => x["brand"]));
 
-        if (Object.keys(request.query).length !== 0) {
-            let filterBrands = Object.keys(request.query);
-            delete filterBrands['sortby'];
-            products = filterAndSort(products, filterBrands, request.query['sortby']);
-        }
-
-        response.render(
-            `${v2ViewsPath}/productpageV2.html`,
-            {
-                ...v2BaseContext,
-                loggedIn: request.session.loggedIn,
-                layout: './basev2.html',
-                products: products,
-                category: "Tablets",
-                subcategoryId: subcategoryId,
-                brands: brands,
-            }
-        );
+    if (Object.keys(request.query).length !== 0) {
+      let filterBrands = Object.keys(request.query);
+      delete filterBrands["sortby"];
+      products = filterAndSort(products, filterBrands, request.query["sortby"]);
     }
+
+    response.render(`${v2ViewsPath}/productpageV2.html`, {
+      ...v2BaseContext,
+      loggedIn: request.session.loggedIn,
+      layout: "./basev2.html",
+      products: products,
+      category: "Tablets",
+      subcategoryId: subcategoryId,
+      brands: brands,
+    });
+  }
 );
 
 app.get(`${v2UrlPath}/laptops`, async (request, response) => {
-    let subcategoryId = "laptops";
-    let products = await database.getProducts({ subcategory: subcategoryId });
-    brands = new Set(products.map((x) => x["brand"]));
-    
-    if (Object.keys(request.query).length !== 0) {
-        let filterBrands = Object.keys(request.query);
-        delete filterBrands['sortby'];
-        products = filterAndSort(products, filterBrands, request.query['sortby']);
-    }
+  let subcategoryId = "laptops";
+  let products = await database.getProducts({ subcategory: subcategoryId });
+  brands = new Set(products.map((x) => x["brand"]));
 
-    response.render(
-        `${v2ViewsPath}/productpageV2.html`,
-        {
-            ...v2BaseContext,
-            userFirstName: request.session.firstName,
-            loggedIn: request.session.loggedIn,
-            layout: './basev2.html',
-            products: products,
-            productsString: JSON.stringify(products),
-            category: "Laptops",
-            subcategoryId: subcategoryId,
-            brands: brands,
-        }
-    );
+  if (Object.keys(request.query).length !== 0) {
+    let filterBrands = Object.keys(request.query);
+    delete filterBrands["sortby"];
+    products = filterAndSort(products, filterBrands, request.query["sortby"]);
+  }
+
+  response.render(`${v2ViewsPath}/productpageV2.html`, {
+    ...v2BaseContext,
+    userFirstName: request.session.firstName,
+    loggedIn: request.session.loggedIn,
+    layout: "./basev2.html",
+    products: products,
+    productsString: JSON.stringify(products),
+    category: "Laptops",
+    subcategoryId: subcategoryId,
+    brands: brands,
+  });
 });
 
 app.get(`${v2UrlPath}/locations`, (request, response) => {
-    response.render(
-        `${v2ViewsPath}/locationsV2.html`,
-        {
-            ...v2BaseContext,
-            layout: false,
-        }
-    )
-})
+  response.render(`${v2ViewsPath}/locationsV2.html`, {
+    ...v2BaseContext,
+    layout: false,
+  });
+});
 
 app.get(`${v2UrlPath}/signin`, (request, response) => {
-    let signInFailed = request.session.signInFailed;
-    request.session.signInFailed = null;
-    response.render(
-        `${v2ViewsPath}/signinV2.html`,
-        {
-            ...v2BaseContext,
-            layout: "./authv2.html",
-            signInFailed: signInFailed,
-        }
-    )
-})
+  let signInFailed = request.session.signInFailed;
+  request.session.signInFailed = null;
+  response.render(`${v2ViewsPath}/signinV2.html`, {
+    ...v2BaseContext,
+    layout: "./authv2.html",
+    signInFailed: signInFailed,
+  });
+});
 
 app.get(`${v2UrlPath}/about`, (request, response) => {
-    response.render(
-        `${v2ViewsPath}/aboutusV2.html`,
-        {
-            ...v2BaseContext,
-            layout: false,
-        }
-    )
-})
+  response.render(`${v2ViewsPath}/aboutusV2.html`, {
+    ...v2BaseContext,
+    layout: false,
+  });
+});
 
 app.get(`${v2UrlPath}/signout`, (request, response) => {
-    request.session.destroy((error)=>{});
-    response.render(
-        `${v2ViewsPath}/signedoutV2.html`,
-        {
-            ...v2BaseContext,
-            layout: "./authv2.html",
-        }
-    );
+  request.session.destroy((error) => {});
+  response.render(`${v2ViewsPath}/signedoutV2.html`, {
+    ...v2BaseContext,
+    layout: "./authv2.html",
+  });
 });
 
 app.get(`${v2UrlPath}/signup`, (request, response) => {
-    let passwordMismatch = request.session.passwordMismatch;
-    request.session.passwordMismatch = null;
-    response.render(
-        `${v2ViewsPath}/createaccountV2.html`,
-        {
-          ...v2BaseContext,
-          layout: "./authv2.html",
-          passwordMismatch: passwordMismatch,
-        }
-    );
+  let passwordMismatch = request.session.passwordMismatch;
+  request.session.passwordMismatch = null;
+  response.render(`${v2ViewsPath}/createaccountV2.html`, {
+    ...v2BaseContext,
+    layout: "./authv2.html",
+    passwordMismatch: passwordMismatch,
+  });
 });
 
 app.get(`${v2UrlPath}/signup/success`, (request, response) => {
-  response.render(
-      `${v2ViewsPath}/createaccountsuccessV2.html`,
-      {
-        ...v2BaseContext,
-        layout: "./authv2.html",
-      }
-  );
+  response.render(`${v2ViewsPath}/createaccountsuccessV2.html`, {
+    ...v2BaseContext,
+    layout: "./authv2.html",
+  });
 });
-
 
 // COMMON
 
-app.post(`${commonUrlPath}/auth`,
-    bodyParser.urlencoded(),
-    async (request, response) => {
-        try {
-            user = await database.getUser({email: request.body.emailaddress, password: request.body.userpassword});
-            response.locals.userId = user['id'];
-            request.session.loggedIn = true;
-            request.session.userId = user['id'];
-            request.session.firstName = user['first_name'];
-            if (request.body.version == 2){
-              response.redirect(v2UrlPath);
-            }
-            else {
-              response.redirect(v1UrlPath);
-            }
-        }
-        catch (error) {
-          request.session.signInFailed = true;
-          if (request.body.version == 2){
-            response.redirect(`${v2UrlPath}/signin`);
-          }
-          else {
-            response.redirect(`${v1UrlPath}/signin`);
-          }
-        }
+app.post(
+  `${commonUrlPath}/auth`,
+  bodyParser.urlencoded(),
+  async (request, response) => {
+    try {
+      user = await database.getUser({
+        email: request.body.emailaddress,
+        password: request.body.userpassword,
+      });
+      response.locals.userId = user["id"];
+      request.session.loggedIn = true;
+      request.session.userId = user["id"];
+      request.session.firstName = user["first_name"];
+      if (request.body.version == 2) {
+        response.redirect(v2UrlPath);
+      } else {
+        response.redirect(v1UrlPath);
+      }
+    } catch (error) {
+      request.session.signInFailed = true;
+      if (request.body.version == 2) {
+        response.redirect(`${v2UrlPath}/signin`);
+      } else {
+        response.redirect(`${v1UrlPath}/signin`);
+      }
     }
+  }
 );
 
-app.post(`${commonUrlPath}/adduser`,
-    bodyParser.urlencoded(),
-    async (request, response) => {
+app.post(
+  `${commonUrlPath}/adduser`,
+  bodyParser.urlencoded(),
+  async (request, response) => {
     if (request.body.userpassword === request.body.reenterpassword) {
-        await database.addUser(
-            request.body.firstname,
-            request.body.lastname,
-            request.body.emailaddress,
-            request.body.userpassword,
-            request.body.address,
-            request.body.city,
-            request.body.state,
-            request.body.country,
-            request.body.zipcode
-        );
+      await database.addUser(
+        request.body.firstname,
+        request.body.lastname,
+        request.body.emailaddress,
+        request.body.userpassword,
+        request.body.address,
+        request.body.city,
+        request.body.state,
+        request.body.country,
+        request.body.zipcode
+      );
 
-        if (request.body.version == 2){
-          response.redirect(`${v2UrlPath}/signup/success`);
-        }
-        else {
-          response.redirect(`${v1UrlPath}/signup/success`);
-        }
+      if (request.body.version == 2) {
+        response.redirect(`${v2UrlPath}/signup/success`);
+      } else {
+        response.redirect(`${v1UrlPath}/signup/success`);
+      }
+    } else {
+      request.session.passwordMismatch = true;
+      if (request.body.version == 2) {
+        response.redirect(`${v2UrlPath}/signup`);
+      } else {
+        response.redirect(`${v1UrlPath}/signup`);
+      }
     }
-    else {
-        request.session.passwordMismatch = true;
-        if (request.body.version == 2){
-            response.redirect(`${v2UrlPath}/signup`);
-        }
-        else {
-            response.redirect(`${v1UrlPath}/signup`);
-        }
-    }
-});
-
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
